d9fe03d49747e123584ad4466cd88f18493e0400
sreyesg <santiagoreyes.g@gmail.com>
2024-07-20 20:58:56 -0700

    end of lab

--- app.js
+++ app.js
@@ -327,15 +327,18 @@ a specific ID 823423 from an array of comment objects.
 - Assign the found comment object to the variable 'commentById'.
 */
 
-let commentById = {}
+let commentById = comments.find((comment) => {
+    return comment.id === 823423 
+
+})
 
 // Complete the exercise in the space below:
 
 
 
 // Check your work:
-// console.log('Exercise 8 my result: ', commentById)
-// console.log('Exercise 8 correct result: ', { text: 'Super good', id: 823423 })
+console.log('Exercise 8 my result: ', commentById)
+console.log('Exercise 8 correct result: ', { text: 'Super good', id: 823423 })
 
 
 /*
@@ -347,7 +350,9 @@ of comment objects.
 - Store the index in the variable 'idx'.
 */
 
-let idx = null
+let idx = comments.findIndex((comment) => {
+    return comment.id === 123523
+})
 
 // Complete the exercise in the space below:
 
